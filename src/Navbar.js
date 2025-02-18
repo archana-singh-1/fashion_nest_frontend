87acb77fecec7c9ebadc888141cc9e47c244a822
Archana Singh <archana21@navgurukul.org>
2025-02-18 11:25:11 +0530

    navbar completed

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,22 @@
 import "./navbar.css"
 function Navbar(){
     return(
+        <>
         <nav>
             <div className="nav">
-                <img src="/home/archana/fashion_nest_frontend/homelogo.png" alt=""/>
+                <div> 
+                    <img src="./homelogo.png" alt="" className="homelogo"/>
+                </div>
                 <input type="text" placeholder="Serach here" className="inputbtn"></input>
+                <img src="./fav.png" alt="" className="fav"></img>
+                <img src="./cart.png" alt="" className="cart"/>
+                <img src="./profile.png" alt="" className="profile"/>
+                
+
             </div>
         </nav>
+        </>
+        
     )
 }
 export default Navbar;
